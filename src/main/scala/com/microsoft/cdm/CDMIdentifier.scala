06685cd6b213ccbaf3aa3d2c832a5079324c708f
Hotzilla <tuukka.lindroos@gmail.com>
2026-06-22 01:37:52 +0300

    Add Spark 3.5 release workflow

--- src/main/scala/com/microsoft/cdm/CDMIdentifier.scala
+++ src/main/scala/com/microsoft/cdm/CDMIdentifier.scala
@@ -1,14 +1,13 @@
 package com.microsoft.cdm
 
 import com.microsoft.cdm.utils.CDMOptions
-import org.apache.spark.sql.catalyst.util.CaseInsensitiveMap
 import org.apache.spark.sql.connector.catalog.Identifier
 import org.apache.spark.sql.util.CaseInsensitiveStringMap
 
 class CDMIdentifier(options: CaseInsensitiveStringMap) extends Identifier{
   private val cdmOptions = new CDMOptions(options) // used to do option validation
 
-  override def namespace(): Array[String] = Array(cdmOptions.storage, cdmOptions.container, cdmOptions.manifestFileName)
+  override def namespace(): Array[String] = Array(cdmOptions.storage, cdmOptions.container, cdmOptions.manifestFileName).filter(_ != null)
 
-  override def name(): String = cdmOptions.entity
+  override def name(): String = Option(cdmOptions.entity).getOrElse("")
 }
